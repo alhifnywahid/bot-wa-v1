9f1a6eddcfaeff30c2ff7691c8a4771aac423548
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-04-10 14:52:33 +0700

    Add files via upload

---
+++ plugins/features/openai2.js
@@ -0,0 +1,27 @@
+const { Configuration, OpenAIApi } = require('openai')
+neoxr.create(async (m, {
+   client,
+   text,
+   prefix,
+   command,
+   Func
+}) => {
+   if (!text) {
+      return client.reply(m.chat, Func.example(prefix, command, 'siapa presiden indonesia sekarang?'), m)
+    }
+    const configuration = new Configuration({
+      apiKey: "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"
+    });
+    const openai = new OpenAIApi(configuration);
+      client.sendReact(m.chat, '🕒', m.key)
+      const response = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: [{role: "user", content: text}],
+          });
+ client.reply(m.chat, `${response.data.choices[0].message.content}`, m)
+}, {
+   usage: ['ai'],
+   use: 'query',
+   category: 'features',
+   limit: true
+}, __filename)
