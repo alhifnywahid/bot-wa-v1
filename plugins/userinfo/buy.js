98973c635dffe76d521b1f53051dca8525751f05
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-04-25 09:36:58 +0700

    Update buy.js

--- plugins/userinfo/buy.js
+++ plugins/userinfo/buy.js
@@ -17,7 +17,15 @@ neoxr.create(async (m, {
          users.point -= price * parseInt(amount)
          users.limitGame += parseInt(amount)
          return client.reply(m.chat, `✅ You have purchased *${amount}* limit with *${Func.h2k(price * parseInt(amount))}* points.`, m)
-      } else if (command == 'buy') {
+      } else if (command == 'buylimit') {
+         if (isNaN(args[0])) return client.reply(m.chat, Func.example(prefix, command, '1'), m)
+         if (args[0] < 1) return client.reply(m.chat, Func.example(prefix, command, '1'), m)
+         if (users.point >= price * parseInt(args[0])) {
+            if ((users.limitGame + parseInt(args[0])) >= maximum) return client.reply(m.chat, Func.texted('bold', `🚩 Limit amount you buy exceeds maximum limit.`), m)
+            users.point -= price * parseInt(args[0])
+            users.limit += parseInt(args[0])
+            return client.reply(m.chat, `✅ You have purchased *${args[0]}* limit with *${Func.h2k(price * args[0])}* points.`, m)
+         } else if (command == 'buylimitgame') {
          if (isNaN(args[0])) return client.reply(m.chat, Func.example(prefix, command, '1'), m)
          if (args[0] < 1) return client.reply(m.chat, Func.example(prefix, command, '1'), m)
          if (users.point >= price * parseInt(args[0])) {
