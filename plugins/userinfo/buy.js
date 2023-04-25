a9e11ef97441e3a1a69dc3e16b9dd8f8be7bd902
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-04-25 11:09:23 +0700

    Update buy.js

--- plugins/userinfo/buy.js
+++ plugins/userinfo/buy.js
@@ -25,52 +25,6 @@ neoxr.create(async (m, {
             users.point -= price * parseInt(args[0])
             users.limit += parseInt(args[0])
             return client.reply(m.chat, `✅ You have purchased *${args[0]}* limit with *${Func.h2k(price * args[0])}* points.`, m)
-         } else if (command == 'buylimitgame') {
-         if (isNaN(args[0])) return client.reply(m.chat, Func.example(prefix, command, '1'), m)
-         if (args[0] < 1) return client.reply(m.chat, Func.example(prefix, command, '1'), m)
-         if (users.point >= price * parseInt(args[0])) {
-            if ((users.limitGame + parseInt(args[0])) >= maximum) return client.reply(m.chat, Func.texted('bold', `🚩 Limit amount you buy exceeds maximum limit.`), m)
-            users.point -= price * parseInt(args[0])
-            users.limitGame += parseInt(args[0])
-            return client.reply(m.chat, `✅ You have purchased *${args[0]}* limit with *${Func.h2k(price * args[0])}* points.`, m)
-         } else {
-            client.reply(m.chat, Func.texted('bold', `🚩 You don't have enough points to buy ${Func.formatNumber(args[0])} limit.`), m)
-         }
-      }
-   } catch (e) {
-      client.reply(m.chat, Func.jsonFormat(e), m)
-   }
-}, {
-   usage: ['buylimit', 'buyall', 'buylimitgame'],
-   category: 'user info'
-}, __filename)
-neoxr.create(async (m, {
-   client,
-   args,
-   prefix,
-   command,
-   users,
-   Func
-}) => {
-   try {
-      let maximum = 100,
-         price = 10000
-      if (users.limitGame >= maximum) return client.reply(m.chat, Func.texted('bold', `🚩 Sorry, you can't buy any more limits because you have reached maximum limit.`), m)
-      if (command == 'buyall') {
-         if (users.point < price) return client.reply(m.chat, Func.texted('bold', `🚩 You don't have enough points to buy limit.`), m)
-         let amount = (users.point / price).toFixed(0)
-         if ((users.limitGame + parseInt(amount)) >= maximum) return client.reply(m.chat, Func.texted('bold', `🚩 Sorry, you can't buy any more limits because you have reached maximum limit.`), m)
-         users.point -= price * parseInt(amount)
-         users.limitGame += parseInt(amount)
-         return client.reply(m.chat, `✅ You have purchased *${amount}* limit with *${Func.h2k(price * parseInt(amount))}* points.`, m)
-      } else if (command == 'buylimit') {
-         if (isNaN(args[0])) return client.reply(m.chat, Func.example(prefix, command, '1'), m)
-         if (args[0] < 1) return client.reply(m.chat, Func.example(prefix, command, '1'), m)
-         if (users.point >= price * parseInt(args[0])) {
-            if ((users.limitGame + parseInt(args[0])) >= maximum) return client.reply(m.chat, Func.texted('bold', `🚩 Limit amount you buy exceeds maximum limit.`), m)
-            users.point -= price * parseInt(args[0])
-            users.limit += parseInt(args[0])
-            return client.reply(m.chat, `✅ You have purchased *${args[0]}* limit with *${Func.h2k(price * args[0])}* points.`, m)
          }
       } else if (command == 'buylimitgame') {
          if (isNaN(args[0])) return client.reply(m.chat, Func.example(prefix, command, '1'), m)
@@ -78,7 +32,7 @@ neoxr.create(async (m, {
          if (users.point >= price * parseInt(args[0])) {
             if ((users.limit + parseInt(args[0])) >= maximum) return client.reply(m.chat, Func.texted('bold', `🚩 Limit amount you buy exceeds maximum limit.`), m)
             users.point -= price * parseInt(args[0])
-            users.limitGame += parseInt(args[0])
+            users.limit += parseInt(args[0])
             return client.reply(m.chat, `✅ You have purchased *${args[0]}* limit with *${Func.h2k(price * args[0])}* points.`, m)
          } else {
             client.reply(m.chat, Func.texted('bold', `🚩 You don't have enough points to buy ${Func.formatNumber(args[0])} limit.`), m)
