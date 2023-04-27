126c0475b28ac147f206832e96a2fe4f8e796ae3
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-04-27 15:02:28 +0700

    Update ai.js

--- plugins/features/ai.js
+++ plugins/features/ai.js
@@ -8,7 +8,18 @@ neoxr.create(async (m, {
 }) => {
    try {
       if (command == 'brainly') {
-         if (!text) return client.reply(m.chat, `${Func.texted('bold', `Fitur  ${command} :`)}\n• adalah fitur tanya jawab, dimana pengguna akan bertanya pada bot dan bot akan menjawab apa yang telah di tanyakan oleh user, bot hanya bisa menjawab pertanyaan text, jadi jika user bertanya dengan gambar BOT otomatis tidak mengerti\n\n${Func.texted('bold', 'Cara Menggunakan fitur :')}\n${Func.example(prefix, 'Perintah', '<isi pertanyaanmu>')}\n${Func.example(prefix, command, 'Siapa penemu pesawat?')}\n\n${Func.texted('bold', `Catatan`)}• Limit : √ \n• Premium : ×`, m)
+         if (!text) return client.reply(m.chat, `${Func.texted('bold', `
+         Fitur  ${command} :`)}
+         • adalah fitur tanya jawab, dimana pengguna akan bertanya pada bot dan bot akan menjawab apa yang telah di tanyakan oleh user, bot hanya bisa menjawab pertanyaan text, jadi jika user bertanya dengan gambar BOT otomatis tidak mengerti
+         
+         ${Func.texted('bold', 'Cara Menggunakan fitur :')}
+         ${Func.example(prefix, 'Perintah', '<isi pertanyaanmu>')}
+         ${Func.example(prefix, command, 'Siapa penemu pesawat?')}
+         
+         ${Func.texted('bold', `Catatan`)}
+         • Limit : √
+         • Premium : ×
+         `, m)
          client.sendReact(m.chat, '🕒', m.key)
          const configuration = new Configuration({
             apiKey: process.env.OPENAI_API_KEY
