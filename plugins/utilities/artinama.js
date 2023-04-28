9a9f6ff19e2e82e1b9f76669169496d747a88f28
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-04-29 05:26:26 +0700

    Create artinama.js

---
+++ plugins/utilities/artinama.js
@@ -0,0 +1,27 @@
+neoxr.create(async (m, {
+   client,
+   text,
+   prefix,
+   command
+   }) => {
+      try {
+         if (!text) return client.reply(m.chat, Func.example(prefix, command, 'megawati'), m)
+         client.sendReact(m.chat, '🕒', m.key)
+         let json = await Api.nama(text)
+         if (!json.status) return client.reply(m.chat, global.status.fail, m)
+         let teks = `◦  *Nama* : ${Func.ucword(text)}\n`
+         teks += `◦  *Arti* : ${json.data.arti.split('arti:')[1]}`
+         client.reply(m.chat, teks, m)
+      } catch {
+         client.reply(m.chat, global.status.error, m)
+      }
+}, {
+   usage: ['artinama'],
+   hidden: ['nama'],
+   use: 'name',
+   category: 'utilities',
+   error: false,
+   restrict: true,
+   premium: true,
+   limit: true
+}, __filename)
