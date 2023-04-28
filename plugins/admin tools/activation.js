28970f6d258a53a6f7ac7aa8d6bf6cd38921944e
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-04-28 17:27:44 +0700

    Create activation.js

---
+++ plugins/admin tools/activation.js
@@ -0,0 +1,33 @@
+neoxr.create(async (m, {
+   args,
+   prefix,
+   command
+   }) => {
+      let gc = global.db.groups.find(v => v.jid == m.chat)
+      let opt = [0, 1]
+      let rows = [{
+         title: 'True',
+         rowId: `${prefix + command} 1`,
+         description: ``
+      }, {
+         title: 'False',
+         rowId: `${prefix + command} 0`,
+         description: ``
+      }]
+      if (!args || !args[0] || !opt.includes(parseInt(args[0]))) return client.sendList(m.chat, '', `🚩 *Current status* : [ ${gc.mute ? 'True' : 'False'} ]`, '', 'Tap!', [{ rows }], m)
+      if (parseInt(args[0]) == 1) {
+         if (gc.mute) return client.reply(m.chat, Func.texted('bold', `🚩 Previously muted.`), m)
+         gc.mute = true
+         client.reply(m.chat, Func.texted('bold', `🚩 Successfully muted.`), m)
+      } else if (parseInt(args[0]) == 0) {
+         if (!gc.mute) return client.reply(m.chat, Func.texted('bold', `🚩 Previously unmuted.`), m)
+         gc.mute = false
+         client.reply(m.chat, Func.texted('bold', `🚩 Successfully unmuted.`), m)
+      }
+}, {
+   usage: ['mute'],
+   use: '0 / 1',
+   category: 'admin tools',
+   group: true,
+   admin: true
+}, __filename)
