d23056688ed30c15699927b9cff29df3d1226683
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-04-29 05:57:58 +0700

    Create hilih.js

---
+++ plugins/utilities/hilih.js
@@ -0,0 +1,20 @@
+neoxr.create(async (m, {
+   client,
+   text,
+   prefix,
+   command
+   }) => {
+      try {
+         let vocal = command.charAt(1)
+         let t = m.quoted ? m.quoted.text : text
+         if (!t) return client.reply(m.chat, Func.example(prefix, command, 'How are you?'), m)
+         client.reply(m.chat, t.toLowerCase().replace(/[aiueo]/gi, vocal), m)
+      } catch (e) {
+         client.reply(m.chat, Func.jsonFormat(e), m)
+      }
+}, {
+   usage: ['halah', 'hilih', 'huluh', 'heleh', 'holoh'],
+   use: 'text or reply chat',
+   category: 'utilities',
+   error: false
+}, __filename)
