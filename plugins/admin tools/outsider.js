c7a6516846ab063c2c8003627ab416c5db8081a4
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-05-02 04:27:26 +0700

    Update outsider.js

--- plugins/admin tools/outsider.js
+++ plugins/admin tools/outsider.js
@@ -3,7 +3,8 @@ neoxr.create(async (m, {
    args,
    prefix,
    command,
-   participants
+   participants,
+   Func
    }) => {
       try {
          let member = participants.filter(v => !v.admin).map(v => v.id).filter(v => !v.startsWith('62') && v != client.decodeJid(client.user.id))
