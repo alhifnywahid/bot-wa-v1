761fdf1f0bd06010ea91a475edcb8eb3836f0abd
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-05-02 15:10:40 +0700

    Create instagram.js

---
+++ plugins/utilities/instagram.js
@@ -0,0 +1,30 @@
+neoxr.create(async (m, {
+   client,
+   args,
+   prefix,
+   command,
+   Func
+   }) => {
+      try {
+         if (!args || !args[0]) return client.reply(m.chat, Func.example(prefix, command, 'https://www.instagram.com/p/CK0tLXyAzEI'), m)
+         if (!args[0].match(/(https:\/\/www.instagram.com)/gi)) return client.reply(m.chat, global.status.invalid, m)
+         client.sendReact(m.chat, '🕒', m.key)
+         let old = new Date()
+         let json = await Api.ig(Func.igFixed(args[0]))
+         if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
+         json.data.map(async v => {
+            client.sendFile(m.chat, v.url, v.type == 'mp4' ? Func.filename('mp4') : Func.filename('jpg'), `🍟 *Fetching* : ${((new Date - old) * 1)} ms`, m)
+            await Func.delay(1500)
+         })
+      } catch (e) {
+         console.log(e)
+         return client.reply(m.chat, global.status.error, m)
+      }
+}, {
+   usage: ['instagram'],
+   hidden: ['igdl'],
+   use: 'link',
+   category: 'utilities',
+   error: false,
+   limit: true
+}, __filename)
