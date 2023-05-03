d2f1e20cb6d0f778fc3902dfff368ef70611bc98
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-05-04 04:14:05 +0700

    Create p.js

---
+++ plugins/miscs/p.js
@@ -0,0 +1,16 @@
+neoxr.create(async (m, {
+   client,
+   Func
+}) => {
+  try {
+    await client.sendSticker(m.chat, await Func.fetchBuffer('./media/sticker/false.webp'), m, {
+      packname: global.db.setting.sk_pack,
+      author: global.db.setting.sk_author
+   })
+  } catch (e) {
+     client.reply(m.chat, Func.jsonFormat(e), m)
+  }
+}, {
+  hidden: ['p'],
+  error: false,
+}, __filename)
