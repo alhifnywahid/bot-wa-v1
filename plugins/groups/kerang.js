f36a8cfea75d8dff0344605e2bfa4c1e812c87df
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-05-04 03:50:46 +0700

    Update kerang.js

--- plugins/groups/kerang.js
+++ plugins/groups/kerang.js
@@ -8,13 +8,13 @@ neoxr.create(async (m, {
    }) => {
      try {
       if (command == 'apakah') {
-      	if (!text) return client.reply(m.chat, Func.example(prefix, command, 'luthfi sangat tampan dan cul'), m)
+      	if (!text) return client.reply(m.chat, Func.example(prefix, command, 'gopret sangat tampan dan cul'), m)
       	let random = Func.random(['Iya', 'Mungkin iya', 'Ya Ndak Tau', 'Kepo Bat Luwh', 'Mungkin Tidak', 'Jelas benar dong', 'Mungkin Benar', 'Jelas Iya Dong', 'Jelas Tidak Dong'])
       	let teks = `*Pertanyaan* : ${command} ${text}\n`
       	teks += `*Jawaban*: ${random}`
       	client.reply(m.chat, teks, m)
       } else if (command == 'kapankah') {
-      	if (!text) return client.reply(m.chat, Func.example(prefix, command, 'luthfi dapat cewek idaman'), m)
+      	if (!text) return client.reply(m.chat, Func.example(prefix, command, 'gopret dapat cewek idaman'), m)
       	let time = Func.randomInt(1, 30)
       	let unit = Func.random(['detik', 'menit', 'jam', 'hari', 'minggu', 'bulan', 'tahun', 'dekade', 'abad'])
       	let teks = `*Pertanyaan* : ${command} ${text}\n`
@@ -28,7 +28,7 @@ neoxr.create(async (m, {
       	teks += `*Jawaban*: @${who.replace(/@.+/, '')}`
     	  client.reply(m.chat, teks, m)
       } else if (command == 'rate') {
-      if (!text) return client.reply(m.chat, Func.example(prefix, command, 'ketampanan mas luthfi'), m)
+      if (!text) return client.reply(m.chat, Func.example(prefix, command, 'ketampanan mas gopret'), m)
       let skor = Func.randomInt(1, 100)
       let teks = `*Pertanyaan* : ${command} ${text}\n`
       teks += `*Jawaban*: ${skor}%`
