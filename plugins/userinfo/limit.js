182c68a9ef2e3e33f92c623c8a069ded433bcbbd
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-05-06 10:34:59 +0700

    Update limit.js

--- plugins/userinfo/limit.js
+++ plugins/userinfo/limit.js
@@ -1,18 +1,28 @@
 neoxr.create(async (m, {
-   client,
-   users,
-   prefix,
-   Func
+  client,
+  users,
+  prefix,
+  Func
 }) => {
-   try {
-      let teks = `➠ Limit : *${Func.formatter(users.limit)}*\n`
-      teks += `➠ Game Limit : *${Func.formatter(users.limitGame)}*`
-      teks += `${!users.premium ? `\n\nUntuk mendapatkan lebih banyak limit, tingkatkan ke paket premium kirim *${prefix}premium*` : ''}`
-      client.reply(m.chat, teks, m)
-   } catch (e) {
-      client.reply(m.chat, Func.jsonFormat(e), m)
-   }
+  const buyPremium = [{
+    buttonId: `${prefix}premium`,
+    buttonText: {
+      displayText: 'BUY PREMIUM'
+    },
+    type: 1
+  }]
+  try {
+     let teks = `➠ Limit : *${Func.formatter(users.limit)}*\n`
+     teks += `➠ Game Limit : *${Func.formatter(users.limitGame)}*`
+     teks += `${!users.premium ? `\n\nUntuk mendapatkan lebih banyak limit, tingkatkan ke paket premium kirim *${prefix}premium*` : ''}`
+     //! SEND MESSAGE WITH TEKS 
+     //client.reply(m.chat, teks, m)
+     //! SEND MESSAGE WITH BUTTONS
+     client.sendButtonText(m.chat, teks, `${global.botname}`, buyPremium)
+  } catch (e) {
+     client.reply(m.chat, Func.jsonFormat(e), m)
+  }
 }, {
-   usage: ['limit'],
-   category: 'user info'
+  usage: ['limit'],
+  category: 'user info'
 }, __filename)
