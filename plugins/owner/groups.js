2f75a09b8f4dce10df8530c32d9c1a030a2f5b4f
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-05-22 23:39:10 +0700

    Update groups.js

--- plugins/owner/groups.js
+++ plugins/owner/groups.js
@@ -1,27 +1,25 @@
 const moment = require('moment-timezone')
 moment.tz.setDefault('Asia/Jakarta').locale('id')
+
 neoxr.create(async (m, {
    client,
    prefix,
    Func
 }) => {
    try {
-      let teks1 = 'now'
+      let teks = 'LIST GROUPS'
       let groupList = async () => Object.entries(await client.groupFetchAllParticipating()).slice(0).map(entry => entry[1])
       let groups = await groupList()
       let rows = []
       groups.map(x => {
          let v = global.db.groups.find(v => v.jid == x.id)
          if (v) {
-            rows.push({
-               title: x.subject,
-               rowId: `${prefix}gc ${x.id}`,
-               description: `[ ${v.stay ? 'FOREVER' : (v.expired == 0 ? 'NOT SET' : Func.timeReverse(v.expired - new Date() * 1))}  | ${x.participants.length} | ${(v.mute ? 'OFF' : 'ON')} | ${moment(v.activity).format('DD/MM/YY HH:mm:ss')} ]`
-            })
+            let description = `${x.subject}\n${prefix}gc ${x.id}\n[ ${v.stay ? 'FOREVER' : (v.expired == 0 ? 'NOT SET' : Func.timeReverse(v.expired - new Date() * 1))}  | ${x.participants.length} | ${(v.mute ? 'OFF' : 'ON')} | ${moment(v.activity).format('DD/MM/YY HH:mm:ss')} ]`
+            rows.push({ description })
          } else {
             global.db.groups.push({
                jid: x.id,
-               activity: new Date * 1,
+               activity: new Date() * 1,
                antidelete: true,
                antilink: true,
                antiporn: true,
@@ -40,10 +38,7 @@ neoxr.create(async (m, {
             })
          }
       })
-      client.reply(m.chat, teks1, m)
-      /*client.sendList(m.chat, '', `Bot has joined to ${groups.length} groups. 🍟`, '', 'Tap!', [{
-         rows
-      }], m)*/
+      client.reply(m.chat, rows, m)
    } catch (e) {
       client.reply(m.chat, Func.jsonFormat(e), m)
    }
