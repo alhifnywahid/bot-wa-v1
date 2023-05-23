e541750a81770a5afd7a9ab96abbf423edb0f6bf
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-05-23 07:12:29 +0700

    Update groups.js

--- plugins/owner/groups.js
+++ plugins/owner/groups.js
@@ -1,25 +1,26 @@
 const moment = require('moment-timezone')
 moment.tz.setDefault('Asia/Jakarta').locale('id')
-
 neoxr.create(async (m, {
    client,
    prefix,
    Func
 }) => {
    try {
-      let teks = 'LIST GROUPS'
       let groupList = async () => Object.entries(await client.groupFetchAllParticipating()).slice(0).map(entry => entry[1])
       let groups = await groupList()
       let rows = []
       groups.map(x => {
          let v = global.db.groups.find(v => v.jid == x.id)
          if (v) {
-            let description = `${x.subject}\n${prefix}gc ${x.id}\n[ ${v.stay ? 'FOREVER' : (v.expired == 0 ? 'NOT SET' : Func.timeReverse(v.expired - new Date() * 1))}  | ${x.participants.length} | ${(v.mute ? 'OFF' : 'ON')} | ${moment(v.activity).format('DD/MM/YY HH:mm:ss')} ]`
-            rows.push({ description })
+            rows.push({
+               title: x.subject,
+               rowId: `${prefix}gc ${x.id}`,
+               description: `[ ${v.stay ? 'FOREVER' : (v.expired == 0 ? 'NOT SET' : Func.timeReverse(v.expired - new Date() * 1))}  | ${x.participants.length} | ${(v.mute ? 'OFF' : 'ON')} | ${moment(v.activity).format('DD/MM/YY HH:mm:ss')} ]`
+            })
          } else {
             global.db.groups.push({
                jid: x.id,
-               activity: new Date() * 1,
+               activity: new Date * 1,
                antidelete: true,
                antilink: true,
                antiporn: true,
@@ -38,7 +39,10 @@ neoxr.create(async (m, {
             })
          }
       })
-      client.reply(m.chat, groups, m)
+      /*client.sendList(m.chat, '', `Bot has joined to ${groups.length} groups. 🍟`, '', 'Tap!', [{
+         rows
+      }], m)*/
+      client.reply(m.chat, global.db.groups.push, m)
    } catch (e) {
       client.reply(m.chat, Func.jsonFormat(e), m)
    }
