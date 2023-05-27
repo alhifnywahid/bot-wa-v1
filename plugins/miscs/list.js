d77a80103bf16f3491a80c824392c250d3a2febc
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-05-27 08:12:37 +0700

    Update list.js

--- plugins/miscs/list.js
+++ plugins/miscs/list.js
@@ -5,34 +5,14 @@ neoxr.create(async (m, {
    args,
    prefix,
    isOwner,
+   command,
    Func
 }) => {
    try {
-      let rows = [{
-         title: 'BANNED',
-         rowId: `${prefix}list 1`,
-         description: ``
-      }, {
-         title: 'ERROR COMMAND',
-         rowId: `${prefix}list 2`,
-         description: ``
-      }, {
-         title: 'INACTIVE PLUGIN',
-         rowId: `${prefix}list 3`,
-         description: ``
-      }, {
-         title: 'PREMIUM',
-         rowId: `${prefix}list 4`,
-         description: ``
-      }, {
-         title: 'PRIVATE CHAT',
-         rowId: `${prefix}list 5`,
-         description: ``
-      }]
       if (!args || !args[0]) return client.sendList(m.chat, '', '🚩 Choose data type you want to see.', '', 'Tap!', [{
          rows
       }], m)
-      if (args[0] == 1) {
+      if (command == 'banlist') {
          const data = global.db.users.filter(v => v.banned)
          if (data.length == 0) return client.reply(m.chat, Func.texted('bold', `🚩 Empty data.`), m)
          let teks = `乂  *B A N L I S T*\n\n`
@@ -42,7 +22,7 @@ neoxr.create(async (m, {
             ads: false,
             largeThumb: true
          })
-      } else if (args[0] == 2) {
+      } else if (command == 'errlist') {
          const data = global.db.setting.error
          if (data.length == 0) return client.reply(m.chat, Func.texted('bold', `🚩 Empty data.`), m)
          let teks = `乂  *E R R L I S T*\n\n`
@@ -52,7 +32,17 @@ neoxr.create(async (m, {
             ads: false,
             largeThumb: true
          })
-      } else if (args[0] == 3) {
+      } else if (command == 'miclist') {
+         const data = global.db.setting.mimic
+         if (data.length == 0) return client.reply(m.chat, Func.texted('bold', `🚩 Empty data.`), m)
+         let teks = `乂  *M I C L I S T*\n\n`
+         teks += data.map(jid => '	◦ @' + jid.replace(/@.+/, '')).join('\n') + '\n\n'
+         teks += global.footer
+         client.sendMessageModify(m.chat, teks, m, {
+            ads: false,
+            largeThumb: true
+         })
+      } else if (command == 'inpluglist') {
          const data = global.db.setting.pluginDisable
          if (data.length == 0) return client.reply(m.chat, Func.texted('bold', `🚩 Empty data.`), m)
          let teks = `乂  *P L U G L I S T*\n\n`
@@ -62,7 +52,7 @@ neoxr.create(async (m, {
             ads: false,
             largeThumb: true
          })
-      } else if (args[0] == 4) {
+      } else if (command == 'premlist') {
          const data = global.db.users.filter(v => v.premium)
          if (data.length == 0) return client.reply(m.chat, Func.texted('bold', `🚩 Empty data.`), m)
          let teks = `乂  *P R E M L I S T*\n\n`
@@ -72,7 +62,7 @@ neoxr.create(async (m, {
             ads: false,
             largeThumb: true
          })
-      } else if (args[0] == 5) {
+      } else if (command == "pclist") {
          if (!isOwner) return client.reply(m.chat, global.status.owner, m)
          const data = global.db.chats.filter(v => v.jid.endsWith('.net'))
          if (data.length == 0) return client.reply(m.chat, Func.texted('bold', `🚩 Empty data.`), m)
@@ -85,9 +75,9 @@ neoxr.create(async (m, {
          })
       }
    } catch (e) {
-      client.reply(m.chat, Func.jsonFormat(e), m)
+      client.reply(m.chat, global.status.error, m)
    }
 }, {
-   usage: ['list'],
+   usage: ['banlist', 'errlist', 'miclist', 'inpluglist', 'premlist', 'pclist'],
    category: 'miscs'
 }, __filename)
