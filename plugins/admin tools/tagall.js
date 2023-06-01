75019bed9a5f6c84d19164023dc4ae88c0d49ffe
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-06-01 09:52:40 +0700

    Create tagall.js

---
+++ plugins/admin tools/tagall.js
@@ -0,0 +1,23 @@
+neoxr.create(async (m, {
+   client,
+   text,
+   prefix,
+   command,
+   participants,
+   Func
+}) => {
+   try {
+let member = participants.map(v => v.id)
+         let readmore = String.fromCharCode(8206).repeat(4001)
+         let message = (!text) ? 'Hello everyone, admin mention you in ' + await (await client.groupMetadata(m.chat)).subject + ' group.' : text
+         client.reply(m.chat, `乂  *E V E R Y O N E*\n\n*“${message}”*\n${readmore}\n${member.map(v => '◦  @' + v.replace(/@.+/, '')).join('\n')}`, m)
+         } catch (e) {
+      client.reply(m.chat, Func.jsonFormat(e), m)
+   }
+}, {
+   usage: ['tagall'],
+   hidden: ['everyone'],
+   category: 'admin tools',
+   admin: true,
+   group: true
+}, __filename)
