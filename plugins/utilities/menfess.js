dce4e9139ec924e522354579d39e6cfce8961c15
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-06-02 06:11:19 +0700

    Update menfess.js

--- plugins/utilities/menfess.js
+++ plugins/utilities/menfess.js
@@ -9,7 +9,7 @@ neoxr.create(async (m, {
    if (!text) return client.reply(m.chat, Func.example(prefix, command, '628xxxxx | asep | i love u'), m)
    let [jid, name, msg] = text.split`|`
    if ((!jid || !name || !msg)) return client.reply(m.chat, Func.example(prefix, command, '628xxxxx | asep | i love u'), m)
-   if (jid == m.sender) return client.reply(m.chat, Func.texted('bold', '🚩 Can\'t send massage.to yourself.'), m)
+   if (jid == m.sender) return client.reply(m.chat, Func.texted('bold', '🚩 Can\'t send message to yourself.'), m)
    let p = (await client.onWhatsApp(jid))[0] || {}
    if (!p.exists) return client.reply(m.chat, Func.texted('bold', '🚩 Number not registered on WhatsApp.'), m)
    let mf = Object.values(global.db.menfess).find(mf => mf.status === true)
@@ -18,24 +18,17 @@ neoxr.create(async (m, {
       let id = +new Date
       let txt = `📩 You got *+1* menfess message from : *${name.trim()}*\n\n`
       txt += `“${msg.trim()}”`
-      await client.sendButtonText(p.jid, txt, global.botname, [{
-         buttonId: `0x199`,
-         buttonText: {
-            displayText: 'Balas Pesan'
-         },
-         type: 1
-      }]).then(() => {
-         client.sendReact(m.chat, '✅', m.key)
-         global.db.menfess[id] = {
-            id,
-            from: m.sender,
-            name,
-            receiver: p.jid,
-            msg,
-            status: false
-         }
-         return !0
-      })
+      await client.reply(p.jid, txt, m)
+      client.sendReact(m.chat, '✅', m.key)
+      global.db.menfess[id] = {
+         id,
+         from: m.sender,
+         name,
+         receiver: p.jid,
+         msg,
+         status: false
+      }
+      return !0
    } catch (e) {
       client.reply(m.chat, Func.jsonFormat(e), m)
    }
