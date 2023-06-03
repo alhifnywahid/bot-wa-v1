07353b458295d8427e58c10efc2f17594abf58a7
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-06-03 13:35:10 +0700

    Create kisahnabi.js

---
+++ plugins/miscs/kisahnabi.js
@@ -0,0 +1,29 @@
+neoxr.create(async (m, {
+   command,
+   text,
+   prefix,
+   client,
+   Func
+}) => {
+      try {
+         if (!text) return client.reply(m.chat, Func.example(prefix, command, 'kisahnabi sulaiman'), m)
+         let url = await fetch(`https://raw.githubusercontent.com/ZeroChanBot/Api-Freee/a9da6483809a1fbf164cdf1dfbfc6a17f2814577/data/kisahNabi/${text}.json`)
+         let kisah = await url.json().catch(_ => "Error")
+         if (kisah == "Error") throw "*Not Found*\n*📮 ᴛɪᴘs :* coba jangan gunakan huruf capital"
+         
+         let hasil = '*Nabi : *' + kisah.name + '\n'
+         hasil += '*Tanggal Lahir *: ' + kisah.thn_kelahiran + '\n'
+         hasil += '*Tempat Lahir : *' + kisah.tmp + '\n'
+         hasil += '*Usia : *' + kisah.usia + '\n\n'
+         hasil += '*— — — — — — — — [ K I S A H ] — — — — — — — —*\n'
+         hasil += kisah.description
+         client.reply(m.chat, hasil, m)
+  } catch (e) {
+      client.reply(m.chat, Func.jsonFormat(e), m)
+   }
+}, {
+   usage: ['kisahnabi'],
+   use: 'query',
+   category: 'islamic',
+   limit: 1,
+}, __filename)
