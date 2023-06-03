4b41f200cec9bc16b480256873287495b66837f6
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-06-03 20:45:58 +0700

    Create wikipedia.js

---
+++ plugins/utilities/wikipedia.js
@@ -0,0 +1,25 @@
+const { wikipedia } = require('@bochilteam/scraper')
+neoxr.create(async (m, {
+   command,
+   text,
+   prefix,
+   client,
+   args,
+   Func
+}) => {
+   try {
+    if (!text) client.reply(m.chat, Func.example(prefix, command, 'penemu lampu?'), m)
+    let json = await wikipedia(text)
+    let sout = json.title + '\n'
+    sout += json.img + '\n'
+    sout += json.articles + '\n'
+    await client.reply(m.chat, sout.trim(), m)
+   } catch (e) {
+      client.reply(m.chat, Func.jsonFormat(e), m)
+   }
+}, {
+   usage: ['wikipedia'],
+   use: 'query',
+   category: 'utilities',
+   limit: 1,
+}, __filename)
