23a4bea038ad71d8be0274a5b13761fee75f2de1
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-06-03 22:22:04 +0700

    Create npmsearch.js

---
+++ plugins/utilities/npmsearch.js
@@ -0,0 +1,31 @@
+const fetch = require('node-fetch')
+neoxr.create(async (m, {
+   command,
+   text,
+   prefix,
+   client,
+   args,
+   Func
+}) => {
+   try {
+    if (!text) return client.reply(m.chat, Func.example(prefix, command, 'list'), m)
+	let res = await fetch(`http://registry.npmjs.com/-/v1/search?text=${text}`)
+	let { objects } = await res.json()
+	if (!objects.length) throw `Query "${text}" not found :/`
+	let txt = objects.map(({ package: pkg }) => {
+        let jo = pkg.name + '\n'
+        jo += pkg.version
+        jo += pkg.links.npm
+        jo += pkg.description
+		return jo
+	}).join`\n\n`
+	m.reply(txt)
+   } catch (e) {
+      client.reply(m.chat, Func.jsonFormat(e), m)
+   }
+}, {
+   usage: ['npmseacrch'],
+   use: 'query',
+   category: 'utilities',
+   limit: 1,
+}, __filename)
