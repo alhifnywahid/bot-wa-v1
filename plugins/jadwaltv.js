f27465edba4f71783eca618e1bd96ec7fa50cb4d
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-06-03 23:11:55 +0700

    Create jadwaltv.js

---
+++ plugins/jadwaltv.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const axios = require('axios')
+const cheerio = require('cheerio')
+neoxr.create(async (m, {
+   command,
+   text,
+   prefix,
+   client,
+   args,
+   Func
+}) => {
+   try {
+    if (!text) return client.reply(m.chat, Func.example(prefix, command, 'Antv'), m)
+	let res = await jadwalTV(text)
+	let txt = res.result.map((v) => `[${v.jam.replace('WIB', ' WIB')}] ${v.acara}`).join`\n`
+    let sout = '*么 J A D W A L  T V*\n\n'
+    sout += res.channel + '\n\n'
+    sout += txt
+	client.reply(m.chat, sout, m)
+   } catch (e) {
+      client.reply(m.chat, Func.jsonFormat(e), m)
+   }
+}, {
+   usage: ['jadwaltv'],
+   use: 'query',
+   category: 'utilities',
+   limit: 1,
+}, __filename)
+
+
+async function jadwalTV(name) {
+	let list = JSON.parse(fs.readFileSync('./src/jadwaltv.json', 'utf-8'))
+	let data = list.find((v) => (new RegExp(name, 'gi')).test(v.channel)), result = []
+	if (!data) throw 'List Channel Yg Tersedia:\n\n' + list.map(v => v.channel).sort().join('\n')
+	let html = (await axios.get(`https://www.jadwaltv.net/${data.isPay ? 'jadwal-pay-tv/' : ''}${data.value}`)).data
+	let $ = cheerio.load(html)
+	$('div > table.table').find('tbody > tr').slice(1).each(function () {
+		let jam = $(this).find('td').eq(0).text()
+		let acara = $(this).find('td').eq(1).text()
+		if (!/Jadwal TV/gi.test(acara) && !/Acara/gi.test(acara)) result.push({ jam, acara })
+	})
+	return { channel: data.channel.toUpperCase(), result }
+}
