0e84fc002ad96882f2e1ebc49d9272ada681478a
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-06-04 14:24:25 +0700

    Update kisahnabi.js

--- plugins/islamic/kisahnabi.js
+++ plugins/islamic/kisahnabi.js
@@ -8,16 +8,17 @@ neoxr.create(async (m, {
 }) => {
       try {
          if (!text) return client.reply(m.chat, Func.example(prefix, command, 'sulaiman'), m)
-         let url = await fetch(`https://raw.githubusercontent.com/ZeroChanBot/Api-Freee/a9da6483809a1fbf164cdf1dfbfc6a17f2814577/data/kisahNabi/${text}.json`)
+         client.sendReact(m.chat, '🕒', m.key)
+         let input = text.toLowerCase();
+         let url = await fetch(`https://raw.githubusercontent.com/ZeroChanBot/Api-Freee/a9da6483809a1fbf164cdf1dfbfc6a17f2814577/data/kisahNabi/${input}.json`)
          let kisah = await url.json().catch(_ => "Error")
-         if (kisah == "Error") throw "*Not Found*\n*📮 ᴛɪᴘs :* coba jangan gunakan huruf capital"
-         
-         let hasil = '*Nabi : *' + kisah.name + '\n'
-         hasil += '*Tanggal Lahir *: ' + kisah.thn_kelahiran + '\n'
-         hasil += '*Tempat Lahir : *' + kisah.tmp + '\n'
-         hasil += '*Usia : *' + kisah.usia + '\n\n'
-         hasil += '*— — — — — — — — [ K I S A H ] — — — — — — — —*\n'
-         hasil += kisah.description
+         if (kisah == "Error") throw "Kisah Nabi yang anda cari tidak ada!"
+         let hasil = '*么  K I S A H - N A B I*\n\n'
+         hasil += '➠ *Nabi : *' + kisah.name + '\n'
+         hasil += '➠ *Tanggal Lahir *: ' + kisah.thn_kelahiran + '\n'
+         hasil += '➠ *Tempat Lahir : *' + kisah.tmp + '\n'
+         hasil += '➠ *Usia : *' + kisah.usia + '\n\n'
+         hasil += '➠ *Kisah* : ' + kisah.description
          client.reply(m.chat, hasil, m)
   } catch (e) {
       client.reply(m.chat, Func.jsonFormat(e), m)
