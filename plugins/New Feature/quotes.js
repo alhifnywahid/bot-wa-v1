b36af9ee4ed212b33e470b97728be105e828e4e8
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-06-09 06:35:26 +0700

    Update quotes.js

---
+++ plugins/New Feature/quotes.js
@@ -0,0 +1,25 @@
+const ds = require('dandi-api');
+neoxr.create(async (m, {
+   command,
+   text,
+   prefix,
+   client,
+   args,
+   Func
+}) => {
+   try {
+      const API = await dhn_api.Quotes();
+      let quotes = '*么  Q U O T E S*\n\n'
+      quotes += 'Author : ' + API.author + '\n'
+      quotes += 'Quotes : ' + API.quotes + '\n\n'
+      quotes += Func.texted('bold', 'Simlple WhatsApp Bot by GOPRET')
+      client.reply(m.chat, quotes, m)
+   } catch (e) {
+      client.reply(m.chat, Func.jsonFormat(e), m)
+   }
+}, {
+   usage: ['quotes'],
+   category: 'utilites',
+   premium: true,
+   limit: 1,
+}, __filename);
