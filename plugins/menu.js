09bddefa765c4fc787c8f5150b72edc440d066c5
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-06-11 09:42:01 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -104,57 +104,60 @@ neoxr.create(async (m, {
                delete client.menu[id]
             }, 180000)
          ]
-      }  else if (style == 3) {
-         let cmd = plugins.filter(v => v.usage && v.category);
-         let category = [];
-         for (let obj of cmd) {
-            if (Object.keys(category).includes(obj.category)) category[obj.category].push(obj);
-            else {
-               category[obj.category] = [];
-               category[obj.category].push(obj);
-            }
-         }
-         const keys = Object.keys(category).sort();
-         let print = 'Ⓟ : Fitur untuk premium.\n';
-         print += 'Ⓛ : Fitur menggunakan limit.';
-         print += '\n' + String.fromCharCode(8206).repeat(4001);
-         for (let k of keys) {
-            print += '\n\n么  *' + k.toUpperCase().split('').map(v => v).join(' ') + '*\n\n';
-            let cmd = plugins.filter(v => v.usage && v.category == k.toLowerCase());
-            if (cmd.length == 0) return;
-            let commands = [];
+      } else if (style == 3) {
+         if (text) {
+            let cmd = plugins.filter(v => v.usage && v.category == text.toLowerCase())
+            if (cmd.length == 0) return client.reply(m.chat, Func.texted('bold', `🚩 Category not available.`), m)
+            let commands = []
             cmd.map(v => {
-               switch (typeof v.usage) {
-                  case 'object':
+               switch (v.usage.constructor.name) {
+                  case 'Array':
                      v.usage.map(x => commands.push({
                         usage: x,
-                        use: v.use ? Func.texted('bold', v.use) : '',
-                        premium: v.premium ? Func.texted('bold', 'Ⓟ') : '',
-                        limit: v.limit ? Func.texted('bold', 'Ⓛ') : ''
-                     }));
-                     break;
-                  case 'string':
+                        use: v.use ? Func.texted('bold', v.use) : ''
+                     }))
+                     break
+                  case 'String':
                      commands.push({
                         usage: v.usage,
-                        use: v.use ? Func.texted('bold', v.use) : '',
-                        premium: v.premium ? Func.texted('bold', 'Ⓟ') : '',
-                        limit: v.limit ? Func.texted('bold', 'Ⓛ') : ''
-                     });
-                     break;
+                        use: v.use ? Func.texted('bold', v.use) : ''
+                     })
                }
-            });
-            print += commands.sort((a, b) => a.usage.localeCompare(b.usage)).map(v => `  ◦  ${prefix + v.usage} ${v.use} ${v.premium} ${v.limit}`).join('\n');
+            })
+            const print = commands.sort((a, b) => a.usage.localeCompare(b.usage)).map((v, i) => {
+               if (i == 0) {
+                  return `┌  ◦  ${prefix + v.usage} ${v.use}`
+               } else if (i == commands.sort((a, b) => a.usage.localeCompare(b.usage)).length - 1) {
+                  return `└  ◦  ${prefix + v.usage} ${v.use}`
+               } else {
+                  return `│  ◦  ${prefix + v.usage} ${v.use}`
+               }
+            }).join('\n')
+            return m.reply(Func.Styles(print, 1))
+         } else {
+            let cmd = plugins.filter(v => v.usage && v.category)
+            let category = []
+            for (let obj of cmd) {
+               if (!obj.category) continue
+               if (Object.keys(category).includes(obj.category)) category[obj.category].push(obj)
+               else {
+                  category[obj.category] = []
+                  category[obj.category].push(obj)
+               }
+            }
+            let rows = []
+            const keys = Object.keys(category).sort()
+            for (let k of keys) {
+               rows.push({
+                  title: k.toUpperCase(),
+                  rowId: `${prefix}menutype ${k}`,
+                  description: ``
+               })
+            }
+            client.sendList(m.chat, '', Func.Styles(message, 1), global.botname, 'Tap!', [{
+               rows
+            }], m)
          }
-         client.menu[id] = [
-            await client.sendMessageModify(m.chat, print + '\n\n' + global.footer, m, {
-               ads: false,
-               largeThumb: true,
-               url: global.db.setting.link
-            }),
-            setTimeout(() => {
-               delete client.menu[id];
-            }, 180000)
-         ];
       } else if (style == 4) {
          let cmd = plugins.filter(v => v.usage && v.category)
          let category = []
@@ -208,6 +211,58 @@ neoxr.create(async (m, {
                delete client.menu[id]
             }, 180000)
          ]
+      }  else if (style == 5) {
+         let cmd = plugins.filter(v => v.usage && v.category);
+         let category = [];
+         for (let obj of cmd) {
+            if (Object.keys(category).includes(obj.category)) category[obj.category].push(obj);
+            else {
+               category[obj.category] = [];
+               category[obj.category].push(obj);
+            }
+         }
+         const keys = Object.keys(category).sort();
+         let print = message;
+         print += '\n\n*𝓟 : Fitur untuk premium.*\n';
+         print += '*𝓛 : Fitur menggunakan limit.*';
+         print += '\n' + String.fromCharCode(8206).repeat(4001);
+         for (let k of keys) {
+            print += '\n\n么  *' + k.toUpperCase().split('').map(v => v).join(' ') + '*\n\n';
+            let cmd = plugins.filter(v => v.usage && v.category == k.toLowerCase());
+            if (cmd.length == 0) return;
+            let commands = [];
+            cmd.map(v => {
+               switch (typeof v.usage) {
+                  case 'object':
+                     v.usage.map(x => commands.push({
+                        usage: x,
+                        use: v.use ? Func.texted('bold', v.use) : '',
+                        premium: v.premium ? Func.texted('bold', '𝓟') : '',
+                        limit: v.limit ? Func.texted('bold', '𝓛') : ''
+                     }));
+                     break;
+                  case 'string':
+                     commands.push({
+                        usage: v.usage,
+                        use: v.use ? Func.texted('bold', v.use) : '',
+                        premium: v.premium ? Func.texted('bold', '𝓟') : '',
+                        limit: v.limit ? Func.texted('bold', '𝓛') : ''
+                     });
+                     break;
+               }
+            });
+            print += commands.sort((a, b) => a.usage.localeCompare(b.usage)).map(v => `  ◦  ${prefix + v.usage} ${v.use} ${v.premium} ${v.limit}`).join('\n');
+         }
+         client.menu[id] = [
+            await client.sendMessageModify(m.chat, print + '\n\n' + global.footer, m, {
+               ads: false,
+               largeThumb: true,
+               url: global.db.setting.link
+            }),
+            setTimeout(() => {
+               delete client.menu[id];
+            }, 180000)
+         ];
       }
    } catch (e) {
       client.reply(m.chat, Func.jsonFormat(e), m)
