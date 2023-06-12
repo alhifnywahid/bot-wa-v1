0d7e329f567680fe0925f40ad30b386bea3081c5
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-06-12 13:09:14 +0700

    Update hentai-jpeg.js

--- plugins/New Feature/hentai-jpeg.js
+++ plugins/New Feature/hentai-jpeg.js
@@ -5,24 +5,34 @@ neoxr.create(
     try {
       if (command == "kitsune") {
         client.sendReact(m.chat, "🕒", m.key);
-        const result = await dh.Anime.kitsune();
-        client.sendFile(m.chat, result, "", `*Dosa tanggung sendiri!*`, m);
+        for (let i = 0; i < 3; i++){
+          const result = await dh.Anime.kitsune();
+          client.sendFile(m.chat, result, "", `*NOTE!!!  DOSA TANGGUNG SENDIRI!*`, m);
+        }
       } else if (command == "hentai") {
         client.sendReact(m.chat, "🕒", m.key);
-        const result = await dh.Anime.hentai();
-        client.sendFile(m.chat, result, "", `*Dosa tanggung sendiri!*`, m);
+        for (let i = 0; i < 3; i++){
+          const result = await dh.Anime.hentai();
+          client.sendFile(m.chat, result, "", `*NOTE!!!  DOSA TANGGUNG SENDIRI!*`, m);
+        }
       } else if (command == "thighs") {
         client.sendReact(m.chat, "🕒", m.key);
-        const result = await dh.Anime.thighs();
-        client.sendFile(m.chat, result, "", `*Dosa tanggung sendiri!*`, m);
+        for (let i = 0; i < 3; i++){
+          const result = await dh.Anime.thighs();
+          client.sendFile(m.chat, result, "", `*NOTE!!!  DOSA TANGGUNG SENDIRI!*`, m);
+        }
       } else if (command == "hanal") {
-        client.sendReact(m.chat, "🕒", m.key);
-        const result = await dh.Anime.hanal();
-        client.sendFile(m.chat, result, "", `*Dosa tanggung sendiri!*`, m);
+        for (let i = 0; i < 3; i++){
+          client.sendReact(m.chat, "🕒", m.key);
+          const result = await dh.Anime.hanal();
+          client.sendFile(m.chat, result, "", `*NOTE!!!  DOSA TANGGUNG SENDIRI!*`, m);
+        }
       } else if (command == "neko") {
         client.sendReact(m.chat, "🕒", m.key);
-        const result = await dh.Anime.neko();
-        client.sendFile(m.chat, result, "", `*Dosa tanggung sendiri!*`, m);
+        for (let i = 0; i < 3; i++){
+          const result = await dh.Anime.neko();
+          client.sendFile(m.chat, result, "", `*NOTE!!!  DOSA TANGGUNG SENDIRI!*`, m);
+        }
       }
     } catch (e) {
       client.reply(m.chat, Func.jsonFormat(e), m);
@@ -32,8 +42,6 @@ neoxr.create(
     usage: ["kitsune", "hentai", "thighs", "hanal", "neko"],
     category: "a new feature",
     premium: true,
-    private: true,
-    owner: false,
     limit: 50,
   },
   __filename
