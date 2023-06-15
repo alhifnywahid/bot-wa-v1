2a6b397c2db75c8088bce4e03e21ee7ebd3391ab
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-06-15 19:08:25 +0700

    Create nomorhoki.js

---
+++ plugins/primbon/nomorhoki.js
@@ -0,0 +1,46 @@
+const { Primbon } = require("scrape-primbon");
+neoxr.create(async (m, {
+  command,
+  text,
+  prefix,
+  client,
+  args,
+  Func
+}) => {
+  try {
+    const primbon = new Primbon();
+    if (!text) return client.reply(m.chat, Func.example(prefix, command, '6285xxxx'), m)
+    primbon.nomer_hoki(text).then((res) => {
+      if (res.status == false) return client.reply(m.chat, '*ERROR! No. Handphone Tidak Valid!*', m)
+      client.sendReact(m.chat, '🕒', m.key)
+      let output = '*么  N O M O R - H O K I*\n\n'
+      output += '*➠ NOMOR* : ' + res.message.nomer_hp + '\n'
+      output += '*➠ ANGKA SHUZI* : ' + res.message.angka_shuzi + '\n\n'
+      output += '*➠ ENERGI POSITIV*\n'
+      output += '* ◦ kekayaan* : ' + res.message.energi_positif.kekayaan + '\n'
+      output += '* ◦ kesehatan* : ' + res.message.energi_positif.kesehatan + '\n'
+      output += '* ◦ cinta* : ' + res.message.energi_positif.cinta + '\n'
+      output += '* ◦ kestabilan* : ' + res.message.energi_positif.kestabilan + '\n'
+      output += '* ◦ persentase* : ' + res.message.energi_positif.persentase + '\n\n'
+      output += '*➠ ENERGI NEGATIV*\n'
+      output += '* ◦ kekayaan* : ' + res.message.energi_negatif.kekayaan + '\n'
+      output += '* ◦ kesehatan* : ' + res.message.energi_negatif.kesehatan + '\n'
+      output += '* ◦ cinta* : ' + res.message.energi_negatif.cinta + '\n'
+      output += '* ◦ kestabilan* : ' + res.message.energi_negatif.kestabilan + '\n'
+      output += '* ◦ persentase* : ' + res.message.energi_negatif.persentase + '\n'
+      output += '*➠ CATATAN* : ' + res.message.catatan + '\n\n'
+      output += global.footer
+      client.reply(m.chat, output, m)
+    });
+  } catch (e) {
+     client.reply(m.chat, Func.jsonFormat(e), m)
+  }
+}, {
+  usage: ['nomorhoki'],
+  use: ['phonenumber'],
+  category: 'primbon',
+  premium: false,
+  private: false,
+  owner: false,
+  limit: 1,
+}, __filename);
