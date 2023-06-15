b2c477692a3817b617244b4c0878f1a89a023df9
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-06-15 17:51:22 +0700

    Create primbon.js

---
+++ plugins/primbon.js
@@ -0,0 +1,32 @@
+const { Primbon } = require("scrape-primbon");
+neoxr.create(async (m, {
+  command,
+  text,
+  prefix,
+  client,
+  args,
+  Func
+}) => {
+  try {
+    const primbon = new Primbon();
+    if (!text) return client.reply(m.chat, Func.example(prefix, command, 'gopret'), m)
+    client.sendReact(m.chat, '🕒', m.key)
+    primbon.arti_nama(text).then((res) => {
+      let output = '*么  A R T I - N A M A*\n\n'
+      output += '*Nama* : ' + res.message.nama + '\n'
+      output += '*Arti* : ' + res.message.arti + '\n\n'
+      output += global.footer
+      client.reply(m.chat, output, m)
+    });
+  } catch (e) {
+     client.reply(m.chat, Func.jsonFormat(e), m)
+  }
+}, {
+  usage: ['artinama'],
+  use: ['query'],
+  category: 'primbon',
+  premium: false,
+  private: false,
+  owner: false,
+  limit: 1,
+}, __filename);
