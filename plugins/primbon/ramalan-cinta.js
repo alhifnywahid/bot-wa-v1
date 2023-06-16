1cafe19e5fccf26f512ecbc145f3ba6021fdc05a
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-06-16 07:09:31 +0700

    Update ramalan-cinta.js

--- plugins/primbon/ramalan-cinta.js
+++ plugins/primbon/ramalan-cinta.js
@@ -11,16 +11,14 @@ neoxr.create(async (m, {
     const primbon = new Primbon();
     let keterangan = '*么  RAMALAN CINTA*\n\n'
     keterangan += '*➠ Keterangan*\n'
-    keterangan += '*Kecocokan cinta pasangan berdasarkan numerologi tanggal lahir.\n\n'
+    keterangan += 'Kecocokan cinta pasangan berdasarkan numerologi tanggal lahir.\n\n'
     keterangan += '*➠ Penggunaan*\n'
-    keterangan += Func.example(prefix, command, 'nama, hari, bulan, tahun, nama, hari, bulan, tahun\n')
+    keterangan += Func.example(prefix, command, 'nama1, hari1, bulan1, tahun1, nama2, hari2, bulan2, tahun2\n')
     keterangan += Func.example(prefix, command, 'hifny, 7, 7, 2000, cindy, 3, 5, 2003')
     if (!text) return client.reply(m.chat, keterangan, m)
     let [name1, hari1, bulan1, tahun1, name2, hari2, bulan2, tahun2] = text.split`,`
-    if (!name1) return client.reply(m.chat, '*Masukkan Nama Anda!*', m)
-    if (!name2) return client.reply(m.chat, '*Masukkan Nama Pasangan Anda!*', m)
     if (!hari1 || isNaN(hari1) || hari1 < 1 || hari1 > 31 || !hari2 || isNaN(hari2) || hari2 < 1 || hari2 > 31) return client.reply(m.chat, '*Hari yang anda masukkan tidak valid!*', m)
-    if (!bulan1 || isNaN(bulan1) || bulan1 < 1 || bulan1 > 12 || !bulan2 || isNaN(bulan2) || bulan2 < 1 || bulan2 > 12) return client.reply(m.chat, '*Bulan yang anda masukkan tidak valid!*', m)
+    if (!hari1 || !hari2 || !bulan1 || isNaN(bulan1) || bulan1 < 1 || bulan1 > 12 || !bulan2 || isNaN(bulan2) || bulan2 < 1 || bulan2 > 12 || !tahun1 || !tahun2) return client.reply(m.chat, `*Error, Mungkin Input Yang Anda Masukkan ada Salah!, Silah ketik ${command}${prefix} untuk melihat cara penggunaan.*`, m)
     client.sendReact(m.chat, '🕒', m.key)
     primbon.ramalan_cinta(name1, hari1, bulan1, tahun1, name2, hari2, bulan2, tahun2).then((res) => {
       if (res.status == false) return client.reply(m.chat, '*Error, Mungkin Input Yang Anda Masukkan Salah!*', m)
@@ -28,7 +26,7 @@ neoxr.create(async (m, {
       output += `➠ *Biodata Anda* : ${res.message.nama_anda.nama}, ${res.message.nama_anda.tgl_lahir}\n`;
       output += `➠ *Biodata Pasangan Anda* : ${res.message.nama_pasangan.nama}, ${res.message.nama_pasangan.tgl_lahir}\n`;
       output += `➠ *Sisi Positif* : ${res.message.sisi_positif}`;
-      output += `➠ *Sisi Negativ* : ${res.message.sisi_negatif}`;
+      output += `➠ *Sisi Negatif* : ${res.message.sisi_negatif}`;
       output += global.footer
       client.reply(m.chat, output, m)
     });
@@ -37,7 +35,7 @@ neoxr.create(async (m, {
   }
 }, {
   usage: ['ramalancinta'],
-  use: ['n1, h1, b1, t1, n2, h2, b2, t2'],
+  use: ['n1,h1,b1,t1,n2,h2,b2,t2'],
   category: 'primbon',
   premium: false,
   private: false,
