888a00d55643d22dee597a1efc5d07270fcc3902
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-06-16 10:46:07 +0700

    Update nomorhoki.js

--- plugins/primbon/nomorhoki.js
+++ plugins/primbon/nomorhoki.js
@@ -29,17 +29,17 @@ neoxr.create(async (m, {
           hasil += `*➠ Nomor Hp* : ${fetchText.split('No. HP : ')[1].split('\n')[0]}\n`
           hasil += `*➠ Angka Bagua Shuzi* : ${fetchText.split('Angka Bagua Shuzi : ')[1].split('\n')[0]}\n\n`
           hasil += `*➠ Energi Positif*\n`
-          hasil += ` *◦ Kekayaan* : ${fetchText.split('Kekayaan = ')[1].split('\n')[0]}\n`
-          hasil += ` *◦ Kesehatan* : ${fetchText.split('Kesehatan = ')[1].split('\n')[0]}\n`
-          hasil += ` *◦ Cinta/Relasi* : ${fetchText.split('Cinta/Relasi = ')[1].split('\n')[0]}\n`
-          hasil += ` *◦ Kestabilan* : ${fetchText.split('Kestabilan =')[1].split('\n')[0]}\n`
-          hasil += ` *◦ Presentase* : ${fetchText.split('%ENERGI NEGATIF')[0].split('% = ')[1]+'%'}\n\n`
+          hasil += `  *◦ Kekayaan* : ${fetchText.split('Kekayaan = ')[1].split('\n')[0]}\n`
+          hasil += `  *◦ Kesehatan* : ${fetchText.split('Kesehatan = ')[1].split('\n')[0]}\n`
+          hasil += `  *◦ Cinta/Relasi* : ${fetchText.split('Cinta/Relasi = ')[1].split('\n')[0]}\n`
+          hasil += `  *◦ Kestabilan* : ${fetchText.split('Kestabilan =')[1].split('\n')[0]}\n`
+          hasil += `  *◦ Presentase* : ${fetchText.split('%ENERGI NEGATIF')[0].split('% = ')[1]+'%'}\n\n`
           hasil += `*➠ Energi Negatif*\n`
-          hasil += ` *◦ Perselisihan* : ${fetchText.split('Perselisihan = ')[1].split('\n')[0]}\n`
-          hasil += ` *◦ Kehilangan* : ${fetchText.split('Kehilangan = ')[1].split('\n')[0]}\n`
-          hasil += ` *◦ Malapetaka* : ${fetchText.split('Malapetaka = ')[1].split('\n')[0]}\n`
-          hasil += ` *◦ Kehancuran* : ${fetchText.split('Kehancuran =')[1].split('\n')[0]}\n`
-          hasil += ` *◦ Presentase* : ${fetchText.split('Kehancuran = ')[1].split('= ')[1].split('\n')[0]}\n\n`
+          hasil += `  *◦ Perselisihan* : ${fetchText.split('Perselisihan = ')[1].split('\n')[0]}\n`
+          hasil += `  *◦ Kehilangan* : ${fetchText.split('Kehilangan = ')[1].split('\n')[0]}\n`
+          hasil += `  *◦ Malapetaka* : ${fetchText.split('Malapetaka = ')[1].split('\n')[0]}\n`
+          hasil += `  *◦ Kehancuran* : ${fetchText.split('Kehancuran =')[1].split('\n')[0]}\n`
+          hasil += `  *◦ Presentase* : ${fetchText.split('Kehancuran = ')[1].split('= ')[1].split('\n')[0]}\n\n`
           hasil += `*➠ Catatan* : ${fetchText.split('* ')[1].split('Masukkan Nomor HP Anda')[0]}\n\n`
           hasil += global.footer
           client.reply(m.chat, hasil, m)
