e8511a60823648086d61e6ff37d2d62d0c013812
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-06-16 10:39:02 +0700

    Update nomorhoki.js

--- plugins/primbon/nomorhoki.js
+++ plugins/primbon/nomorhoki.js
@@ -1,4 +1,5 @@
-const { Primbon } = require("scrape-primbon");
+const axios = require('axios');
+const cheerio = require('cheerio');
 neoxr.create(async (m, {
   command,
   text,
@@ -8,32 +9,42 @@ neoxr.create(async (m, {
   Func
 }) => {
   try {
-    const primbon = new Primbon();
     if (!text) return client.reply(m.chat, Func.example(prefix, command, '6285xxxx'), m)
     client.sendReact(m.chat, '🕒', m.key)
-    primbon.nomer_hoki(text).then((res) => {
-      if (res.status == false) return client.reply(m.chat, '*ERROR! No. Handphone Tidak Valid!*', m)
-      let output = '*么  N O M O R - H O K I*\n\n'
-      output += '*➠ NOMOR* : ' + res.message.nomer_hp + '\n'
-      output += '*➠ ANGKA SHUZI* : ' + res.message.angka_shuzi + '\n\n'
-      output += '*➠ ENERGI POSITIV*\n'
-      output += ' *◦ kekayaan* : ' + res.message.energi_positif.kekayaan + '\n'
-      output += ' *◦ kesehatan* : ' + res.message.energi_positif.kesehatan + '\n'
-      output += ' *◦ cinta* : ' + res.message.energi_positif.cinta + '\n'
-      output += ' *◦ kestabilan* : ' + res.message.energi_positif.kestabilan + '\n'
-      output += ' *◦ persentase* : ' + res.message.energi_positif.persentase + '\n\n'
-      output += '*➠ ENERGI NEGATIV*\n'
-      output += ' *◦ perselisihan* : ' + res.message.energi_negatif.perselisihan + '\n'
-      output += ' *◦ kehilangan* : ' + res.message.energi_negatif.kehilangan + '\n'
-      output += ' *◦ malapetaka* : ' + res.message.energi_negatif.malapetaka + '\n'
-      output += ' *◦ kehancuran* : ' + res.message.energi_negatif.kehancuran + '\n'
-      output += ' *◦ persentase* : ' + res.message.energi_negatif.persentase + '\n'
-      output += '*➠ CATATAN* : ' + res.message.catatan + '\n\n'
-      output += global.footer
-      client.reply(m.chat, output, m)
-    });
+    const url = 'https://primbon.com/no_hoki_bagua_shuzi.php';
+    const data = new URLSearchParams();
+    data.append('nomer', text);
+    data.append('submit', 'Submit');
+    
+    axios.post(url, data, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+  }).then(response => {
+          const html = response.data;
+          const $ = cheerio.load(html);
+          let fetchText = $('#body').text().trim()
+          let hasil = '*么  N O M O R - H O K I*\n\n'
+          hasil += `*➠ Nomor Hp* : ${fetchText.split('No. HP : ')[1].split('\n')[0]}\n`
+          hasil += `*➠ Angka Bagua Shuzi* : ${fetchText.split('Angka Bagua Shuzi : ')[1].split('\n')[0]}\n\n`
+          hasil += `*➠ Energi Positif*\n`
+          hasil += ` *◦ Kekayaan* : ${fetchText.split('Kekayaan = ')[1].split('\n')[0]}\n`
+          hasil += ` *◦ Kesehatan* : ${fetchText.split('Kesehatan = ')[1].split('\n')[0]}\n`
+          hasil += ` *◦ Cinta/Relasi* : ${fetchText.split('Cinta/Relasi = ')[1].split('\n')[0]}\n`
+          hasil += ` *◦ Kestabilan* : ${fetchText.split('Kestabilan =')[1].split('\n')[0]}\n`
+          hasil += ` *◦ Presentase* : ${fetchText.split('%ENERGI NEGATIF')[0].split('% = ')[1]+'%'}\n\n`
+          hasil += `*➠ Energi Negatif*\n`
+          hasil += ` *◦ Perselisihan* : ${fetchText.split('Perselisihan = ')[1].split('\n')[0]}\n`
+          hasil += ` *◦ Kehilangan* : ${fetchText.split('Kehilangan = ')[1].split('\n')[0]}\n`
+          hasil += ` *◦ Malapetaka* : ${fetchText.split('Malapetaka = ')[1].split('\n')[0]}\n`
+          hasil += ` *◦ Kehancuran* : ${fetchText.split('Kehancuran =')[1].split('\n')[0]}\n`
+          hasil += ` *◦ Presentase* : ${fetchText.split('Kehancuran = ')[1].split('= ')[1].split('\n')[0]}\n\n`
+          hasil += `*➠ Catatan* : ${fetchText.split('* ')[1].split('Masukkan Nomor HP Anda')[0]}\n\n`
+          hasil += global.footer
+          client.reply(m.chat, hasil, m)
+      })
   } catch (e) {
-     client.reply(m.chat, Func.jsonFormat(e), m)
+     client.reply(m.chat, '*ERROR! No. Handphone Tidak Valid!*', m)
   }
 }, {
   usage: ['nomorhoki'],
