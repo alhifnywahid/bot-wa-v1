4b43187674632c04a3fe562010d88a71f7ee8a0c
alhifny-wahid <117329188+alhifny-wahid@users.noreply.github.com>
2023-06-18 21:04:36 +0700

    Create xnxx-search.js

---
+++ plugins/searching/xnxx-search.js
@@ -0,0 +1,28 @@
+const fg = require('api-dylux');
+neoxr.create(async (m, {
+  command,
+  text,
+  prefix,
+  client,
+  args,
+  Func
+}) => {
+  try {
+    let jo = await fg.xnxxSearch(text)
+     client.reply(m.chat, jo, m)
+  } catch (e) {
+     client.reply(m.chat, Func.jsonFormat(e), m)
+  }
+}, {
+  usage: ['vicsearch'],
+  use: ['query'],
+  category: 'a new feature',
+  premium: false,
+  private: false,
+  owner: false,
+  limit: 1,
+}, __filename);
+
+
+
+
